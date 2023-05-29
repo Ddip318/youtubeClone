446a61102baf72512c64956f838f4df7ac718f0a
Ddip318 <wadrt175t@gmail.com>
2023-05-29 22:25:29 +0900

    basic middleware
    기초 미들웨어 활용

--- src/server.js
+++ src/server.js
@@ -4,11 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send("i have a kimbob");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url == "/protected") {
+    return res.send("<h1>Not Allowd<h1>");
+  }
+  next();
 };
 
+const handleHome = (req, res) => {
+  console.log("Home");
+  return res.end();
+};
+const handleProtected = (req, res) => {
+  console.log("welcome protected");
+  return res.end();
+};
+
+app.use(logger);
+//app.use(privateMiddleware);
 app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Access : ${PORT}`);
 
