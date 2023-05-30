a500ba66a9cf6cffb8186014390ef1516d15e77e
Ddip318 <wadrt175t@gmail.com>
2023-05-30 19:13:02 +0900

    Using FakeUser
    가상의 유저를 활용한 pug 변수사용가능 확인

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,23 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+const fakeUser = {
+  username: "Nico",
+  loggedIn: true,
+};
+
+export const trending = (req, res) => {
+  res.render("home", { pageTitle: "Home", fakeUser });
+};
 export const search = (req, res) => res.send("Search");
 
 export const see = (req, res) => {
-  res.send(`watch Video #${req.params.id}`);
+  //res.send(`watch Video #${req.params.id}`);
+  res.render("watch", { pageTitle: "Watch" });
 };
 export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("Edit Video");
+  res.render("edit", { pageTitle: "Edit" });
 };
-export const upload = (req, res) => res.send("upload");
+export const upload = (req, res) => {
+  res.send("upload");
+};
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   res.send("deleteVideo");
