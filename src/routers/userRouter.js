6916f9deb4b03e3fc137bc454c5eab826236c505
Ddip318 <wadrt175t@gmail.com>
2023-06-03 18:45:04 +0900

    finsih social login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  getEdit,
+  postEdit,
+  logout,
+  see,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/:id", see);
+
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
