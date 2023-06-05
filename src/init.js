36cb5392d775810a272bc11f02c01f57241d4cd9
Ddip318 <wadrt175t@gmail.com>
2023-06-05 18:16:08 +0900

    Make a Recorder

--- src/init.js
+++ src/init.js
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import "./db";
 import "./models/Video";
+import "./models/User";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
-const handleListening = () => console.log(`Server listening on ${PORT}`);
+const handleListening = () =>
+  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+
 app.listen(PORT, handleListening);
 // callback : 대기를 하고 Then(render이후 마지막)실행 <-> promise
